a02edaa708a2dcc12a1f8e5b004cc1f4438bb62a
Areeb Mohammed <am0247@uah.edu>
2022-10-25 12:00:59 -0500

    Finished navbar outline

--- src/components/AstraNavbar/AstraNavbar.jsx
+++ src/components/AstraNavbar/AstraNavbar.jsx
@@ -1,10 +1,20 @@
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function AstraNavbar() {
     return (
-        <Navbar className="bg-primary">
+        <Navbar className="bg-light" expand="md">
             <Container>
-                <Navbar.Brand>ASTRA</Navbar.Brand>
+                <Navbar.Brand className="">ASTRA</Navbar.Brand>
+                <Navbar.Toggle aria-controls="astra-navbar-nav" />
+                <Navbar.Collapse id="astra-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link>Status</Nav.Link>
+                        <Nav.Link>Rover Control</Nav.Link>
+                        <Nav.Link>Arm Control</Nav.Link>
+                        <Nav.Link>Biosensor Control</Nav.Link>
+                        <Nav.Link>Autonomous Control</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
