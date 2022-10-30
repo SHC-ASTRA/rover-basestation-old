fe9b567f83867c2ba814cd1418b69640eb2736a3
jdww5567 <jadaniel06@gmail.com>
2022-10-30 18:01:42 -0500

    Embed Arm Control Page

--- src/pages/Arm-Control/Arm-Control.jsx
+++ src/pages/Arm-Control/Arm-Control.jsx
@@ -3,7 +3,9 @@ import { Container } from 'react-bootstrap';
 function ArmControl() {
     return (
         <Container className="p-3" fluid>
-            Arm
+            <div id = "wrapFrame">
+                <iframe id = "frame" src="http://localhost:5174/" height = "100%" width = "100%"></iframe>
+            </div>
         </Container>
     );
 }
