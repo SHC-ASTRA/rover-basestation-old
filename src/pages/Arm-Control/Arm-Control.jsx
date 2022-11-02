533600bee38ee1b24d06092b1c2b76dbb289dac1
jdww5567 <jadaniel06@gmail.com>
2022-11-02 02:51:50 -0500

    Fixed height of iframe

--- src/pages/Arm-Control/Arm-Control.jsx
+++ src/pages/Arm-Control/Arm-Control.jsx
@@ -1,12 +1,10 @@
 import { Container } from 'react-bootstrap';
-import IframeResizer from 'iframe-resizer-react'
 
 function ArmControl() {
     return (
-        
         <Container className="p-3" fluid>
-            <div>
-                <iframe src = {import.meta.env.VITE_ARM_URL}/>
+            <div style={{height: '105vh'}}>
+                <iframe src = {import.meta.env.VITE_ARM_URL} width = "100%" height = "100%"/>
             </div>
         </Container>
     );
