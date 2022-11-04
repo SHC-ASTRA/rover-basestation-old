6e7e855ffbd0eb0be41cf65bf89c6ca4464dd791
Areeb Mohammed <am0247@uah.edu>
2022-11-03 20:41:17 -0500

    Added modal to control connecting to ROS

--- src/components/AstraNavbar/NavbarRosStatus.jsx
+++ src/components/AstraNavbar/NavbarRosStatus.jsx
@@ -2,13 +2,16 @@ import { useContext, useState } from 'react';
 import ros from '../../utilities/ROS/ROS';
 import { RosContext } from '../../utilities/ROS/RosContext';
 import { RecordCircleFill } from 'react-bootstrap-icons';
+import RosModal from '../RosModal/RosModal';
 
 function NavbarRosStatus() {
     const { rosState, rosError } = useContext(RosContext);
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => setShowModal(true);
+    const closeModal = () => setShowModal(false);
 
     var statusColor, statusMessage;
-
-    console.log(rosState, rosError);
 
     if (rosError) {
         statusColor = 'yellow';
@@ -21,18 +24,21 @@ function NavbarRosStatus() {
         statusMessage = 'Connected';
     }
 
-    var onClick = () => {
-        if (rosState == 'Disconnected') {
-            ros.connect('ws://localhost:9090');
-        } else if (rosState == 'Connected') {
-            ros.close();
-        }
-    };
+    // var onClick = () => {
+    //     if (rosState == 'Disconnected') {
+    //         ros.connect('ws://localhost:9090');
+    //     } else if (rosState == 'Connected') {
+    //         ros.close();
+    //     }
+    // };
 
     return (
-        <div onClick={onClick}>
-            <RecordCircleFill color={statusColor} size={20} />
-            <span>{statusMessage}</span>
+        <div>
+            <div onClick={openModal}>
+                <RecordCircleFill color={statusColor} size={20} />
+                <span>{statusMessage}</span>
+            </div>
+            <RosModal show={showModal} closeModal={closeModal} />
         </div>
     );
 }
---
+++ src/components/RosModal/RosModal.jsx
@@ -0,0 +1,109 @@
+import { createRef, useContext, useEffect, useState } from 'react';
+import {
+    Container,
+    Modal,
+    Row,
+    Col,
+    Form,
+    InputGroup,
+    Button,
+} from 'react-bootstrap';
+import ros from '../../utilities/ROS/ROS';
+import { RosContext } from '../../utilities/ROS/RosContext';
+
+function RosModal({ show, closeModal }) {
+    const { rosState } = useContext(RosContext);
+    // References to form inputs
+    const webBridgeAddress = createRef();
+    const webBridgePort = createRef();
+
+    // Store the state of the form
+    const [formState, setFormState] = useState({
+        address: 'localhost',
+        port: '9090',
+    });
+
+    // Update if there is a valid reference
+    const updateFormState = (addr, port) => {
+        if (addr.current) {
+            setFormState({
+                address: addr.current.value,
+                port: port.current.value,
+            });
+        }
+    };
+
+    // Only update when the show state changes (when the modal dis/appears)
+    useEffect(() => {
+        updateFormState(webBridgeAddress, webBridgePort);
+    }, [show]);
+
+    // Connect Button Configuration
+    var connectButtonVariant, connectButtonMessage;
+    if (rosState == 'Disconnected') {
+        connectButtonVariant = 'success';
+        connectButtonMessage = 'Connect';
+    } else {
+        connectButtonVariant = 'danger';
+        connectButtonMessage = 'Disconnect';
+    }
+    const connectButtonClick = () => {
+        if (rosState == 'Disconnected') {
+            var url = `ws://${webBridgeAddress.current.value}:${webBridgePort.current.value}`;
+            ros.connect(url);
+        } else {
+            ros.close();
+        }
+    };
+
+    return (
+        <Modal show={show} onHide={closeModal} size="lg">
+            <Modal.Header>
+                <Modal.Title>ROS Connection</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Container>
+                    <Row>
+                        <Col>List of Previous Connections</Col>
+                        <Col>
+                            <Form>
+                                <Form.Group controlId="formRosConnect">
+                                    <Form.Label>
+                                        ROSBridge Server Address
+                                    </Form.Label>
+                                    <div className="d-grid gap-2">
+                                        <InputGroup>
+                                            <InputGroup.Text>
+                                                ws://
+                                            </InputGroup.Text>
+                                            <Form.Control
+                                                type="text"
+                                                ref={webBridgeAddress}
+                                                defaultValue={formState.address}
+                                            />
+                                            <InputGroup.Text>:</InputGroup.Text>
+                                            <Form.Control
+                                                type="number"
+                                                ref={webBridgePort}
+                                                defaultValue={formState.port}
+                                                style={{ maxWidth: 85 }}
+                                            />
+                                        </InputGroup>
+                                        <Button
+                                            variant={connectButtonVariant}
+                                            onClick={connectButtonClick}
+                                        >
+                                            {connectButtonMessage}
+                                        </Button>
+                                    </div>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+export default RosModal;
