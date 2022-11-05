28e3e77e43b88dc0435d19eed8875eec55cb6752
Nick McGarry <94426110+nlive65@users.noreply.github.com>
2022-11-05 12:17:40 -0500

    Update Status.jsx
    Added a new button and some styling for it

--- src/pages/Status/Status.jsx
+++ src/pages/Status/Status.jsx
@@ -1,6 +1,41 @@
 import { Container } from 'react-bootstrap';
+import {useState,props} from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './Status.css';
+
+
+function modeButtons(){
+    const [mode, setMode] = useState("Manual");//replace with current rover mode  
+    let styleBool = false;
+    switch(mode){
+        case "Auto":
+            console.log("Auto");
+            styleBool = true;
+            break;
+        case "Manual":
+            console.log("Manual");
+            styleBool = false;
+            break;
+        default:
+            console.log("No option");
+            break;
+    }
+    return(
+        <div className='card' style={{width: '21rem'}}>
+            <h5 className="card-title text-center">Current Mode: {mode}</h5>
+            <div className='card-body'>
+                <div className='modeButtons'>
+                    <button onClick={()=>setMode("Auto")} style={{backgroundColor: styleBool ? "green" : '#696e69'}} className='selector'>Auto</button>
+                    <button onClick={()=>setMode("Manual")} style={{backgroundColor: styleBool ? '#696e69' : "green"}} className='selector'>Manual</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
 
 function Status() {
     return (
@@ -128,6 +163,12 @@ function Status() {
                         </div>
                     </div>
                 </Col>
+            </Row>
+            <Row>
+                <Col>
+                {modeButtons()}
+                </Col>
+                <Col></Col>
             </Row>
         </Container>
     );
