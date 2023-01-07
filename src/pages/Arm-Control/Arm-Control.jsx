b65c4540844ee4b0e59706a9ed3ad0b8d4495044
jdww5567 <jadaniel06@gmail.com>
2023-01-07 17:34:53 -0600

    Full layout of status page and defaulted useStates

--- src/pages/Arm-Control/Arm-Control.jsx
+++ src/pages/Arm-Control/Arm-Control.jsx
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap';
 
 function ArmControl() {
     return (
-            <Container style={{height: '89vh'}}>
+            <Container style = {{height: '89vh'}}>
                 <iframe src = {import.meta.env.VITE_ARM_URL} width = "100%" height = "99%"/>
             </Container>
     );
--- src/pages/Status/Status.jsx
+++ src/pages/Status/Status.jsx
@@ -1,133 +1,192 @@
 import { Container } from 'react-bootstrap';
+import { useState } from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './Status.css';
+
+function statusIndicator(name) {
+    const [status, setStatus] = useState(true);
+
+    return(
+        <div style = {{backgroundColor: status ? "green" : "red", width: "100%"}}>
+            {name}
+        </div>
+    )
+}
+
+function diagnostics() {
+    return(
+        <div className = "card">
+            <h5 className="card-title text-center">
+                Diagnostics
+            </h5>
+            <div className = "card-body">
+                <div className = "d-grid gap-2">
+                    {statusIndicator("Coms")}
+                    {statusIndicator("Arm")}
+                    {statusIndicator("Bio Sensor")}
+                    {statusIndicator("Drone")}
+                    {statusIndicator("LiDar")}
+                    {statusIndicator("IMU")}
+                    {statusIndicator("Teensy")}
+                    {statusIndicator("Controller")}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function percentBar(name) {
+    const [percent, setPercent] = useState(40)
+
+    let rectHeight = 100 - percent
+    rectHeight = rectHeight + "%"
+
+    return(
+        <div className = "card">
+            <h6 className="card-title text-center">
+                {name}
+            </h6>
+            <svg className = "card-body" style = {{backgroundColor: "green", padding: "2px"}}>
+                <rect height = {rectHeight} width = "100%" style = {{fill: "rgb(255,255,255)"}}/>
+            </svg>
+            {percent + "%"}
+        </div>
+    )
+}
+
+function usage() {
+    return(
+        <div className = "card">
+            <h5 className="card-title text-center">
+                Usage
+            </h5>
+            <div className = "card-body">
+                <Row>
+                    <Col>
+                        {percentBar("CPU")}
+                    </Col>
+                    <Col>
+                        {percentBar("GPU")}
+                    </Col>
+                    <Col>
+                        {percentBar("RAM")}
+                    </Col>
+                </Row>
+            </div>
+        </div>
+    )
+}
+
+function gps() {
+    const [metrics, setMetrics] = useState([0,0,0,0,0])
+
+    return(
+        <div className = "card">
+            <h5 className="card-title text-center">
+                GPS
+            </h5>
+            <div className = "card-body">
+                <div className = "d-grid gap-2">
+                    <div>
+                        Latitude: {metrics[0]}°
+                    </div>
+                    <div>
+                        Longitude: {metrics[1]}°
+                    </div>
+                    <div>
+                        Altitude: {metrics[2]}m
+                    </div>
+                    <div>
+                        Accuracy: {metrics[3]}m
+                    </div>
+                    <div>
+                        Time: {metrics[4]}s
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function battery() {
+    const [metrics, setMetrics] = useState([0,0]);
+
+    return(
+        <div className = "card">
+            <h5 className="card-title text-center">
+                Battery
+            </h5>
+            <div className = "card-body">
+                <div className = "d-grid gap-2">
+                    <div>
+                        Charge: {metrics[0]}%
+                    </div>
+                    <div>
+                        Voltage: {metrics[1]}V
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+// function modeButtons(){
+//     const [mode, setMode] = useState("Manual");//replace with current rover mode 
+
+//     let styleBool = false;
+
+//     switch(mode){
+//         case "Auto":
+//             console.log("Auto");
+//             styleBool = true;
+//             break;
+//         case "Manual":
+//             console.log("Manual");
+//             styleBool = false;
+//             break;
+//         default:
+//             console.log("No option");
+//             break;
+//     }
+    
+//     return(
+//         <div className='card' style={{width: '21rem'}}>
+//             <h5 className="card-title text-center">Current Mode: {mode}</h5>
+//             <div className='card-body'>
+//                 <div className='modeButtons'>
+//                     <button onClick={()=>setMode("Auto")} style={{backgroundColor: styleBool ? "green" : '#696e69'}} className='selector'>Auto</button>
+//                     <button onClick={()=>setMode("Manual")} style={{backgroundColor: styleBool ? '#696e69' : "green"}} className='selector'>Manual</button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+
+
 
 function Status() {
     return (
         <Container className = "p-2">
             <Row>
                 <Col>
-                    <div className = "card">
-                        <h5 className="card-title text-center">
-                            GPS
-                        </h5>
-                        <div className = "card-body">
-                            <div className = "d-grid gap-2">
-                                <div>
-                                    Latitude:
-                                </div>
-                                <div>
-                                    Longitude: 
-                                </div>
-                                <div>
-                                    Altitude: 
-                                </div>
-                                <div>
-                                    Accuracy: 
-                                </div>
-                                <div>
-                                    Time: 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {gps()}
                 </Col>
                 <Col>
-                    <div className = "card">
-                        <h5 className="card-title text-center">
-                            Performance
-                        </h5>
-                        <div className = "card-body">
-                            <Row>
-                                <Col>
-                                    <div className = "card">
-                                        <h6 className="card-title text-center">
-                                            CPU
-                                        </h6>
-                                        <div className = "card-body">
-                                            <div id = 'cpuStatus'>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Col>
-                                <Col>
-                                    <div className = "card">
-                                        <h6 className="card-title text-center">
-                                            GPU
-                                        </h6>
-                                        <div className = "card-body">
-                                            <div id = 'gpuStatus'>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Col>
-                                <Col>
-                                    <div className = "card">
-                                        <h6 className="card-title text-center">
-                                            RAM
-                                        </h6>
-                                        <div className = "card-body">
-                                            <div id = 'memoryStatus'> 
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </div>
-                    </div>
+                    {usage()}
                 </Col>
                 <Col>
-                    <div className = "card">
-                        <h5 className="card-title text-center">
-                            Battery
-                        </h5>
-                        <div className = "card-body">
-                            <div className = "d-grid gap-2">
-                                <div>
-                                    Charge: 
-                                </div>
-                                <div>
-                                    Voltage: 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {diagnostics()}
                 </Col>
+            </Row>
+            <Row>
                 <Col>
-                    <div className = "card">
-                        <h5 className="card-title text-center">
-                            Diagnostics
-                        </h5>
-                        <div className = "card-body">
-                            <div className = "d-grid gap-2">
-                                <div>
-                                    Coms: 
-                                </div>
-                                <div>
-                                    Arm: 
-                                </div>
-                                <div>
-                                    Bio Sensor: 
-                                </div>
-                                <div>
-                                    Drone: 
-                                </div>
-                                <div>
-                                    LiDar: 
-                                </div>
-                                <div>
-                                    IMU: 
-                                </div>
-                                <div>
-                                    Teensy: 
-                                </div>
-                                <div>
-                                    Controller: 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Col>
+                    {battery()}
+                </Col> 
+                {/* <Col>
+                    {modeButtons()}
+                </Col> */}
             </Row>
         </Container>
     );
