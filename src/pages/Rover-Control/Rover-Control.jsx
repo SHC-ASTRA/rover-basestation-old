4f32bff5da9374c5e8c5971e18a3ec40d8a711b1
Nick <nbm0012@uah.edu>
2023-01-21 12:17:01 -0600

    Added Leaflet to rover control for rover map position
    Currently just running example code but will be switched to offline tile layer

--- src/pages/Rover-Control/Rover-Control.jsx
+++ src/pages/Rover-Control/Rover-Control.jsx
@@ -1,10 +1,14 @@
 import { Container } from 'react-bootstrap';
-
+import { MapContainer,TileLayer,useMap,Marker,Popup } from 'react-leaflet';
 function RoverControl() {
-    return (
-        <Container className = "p-2">
-            Rover
-        </Container>
+    return (<>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height:300,width:500}}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+</MapContainer>  
+    </>
     );
 }
 
