accca021c7ed1fe5f4389069c3eb82742fd14506
jdww5567 <jadaniel06@gmail.com>
2023-02-04 11:50:12 -0600

    Carousel

--- src/pages/Biosensor-Control/Biosensor-Control.jsx
+++ src/pages/Biosensor-Control/Biosensor-Control.jsx
@@ -1,5 +1,18 @@
 import { Container, Card, InputGroup, Form, Button, ButtonGroup, Dropdown } from 'react-bootstrap'
 import { useState, useEffect, useRef } from 'react'
+import ros from '../../utilities/ROS/ROS'
+
+var positions =
+{
+  "separator1": 1,
+  "separator2": 2,
+  "separator3": 3,
+  "dosing1": 4,
+  "dosing2": 5,
+  "capping": 6,
+  "observation": 6,
+  "0": 0
+}
 
 function inputDouble(name, first, second, doubleRef, textWidth) {
     const doubleRefAgain = useRef()
@@ -90,9 +103,41 @@ function inputGo(name, goRef, textWidth) {
     );
 }
 
+function rosFeed() {
+    const updateFeed = (message) => {
+        var time = new Date().toTimeString().split(' ')[0];
+        ROSFeed.val('[' + time + '] ' + message + '\n' + ROSFeed.val());
+    };
+
+    var bioStatusSub = new ROSLIB.Topic({
+        ros: ros,
+        name: '/bio/status',
+        messageType: 'std_msgs/String'
+    });
+    bioStatusSub.subscribe(updateFeed)
+
+    return(
+        <Card style = {{width: "100%"}}>
+            <Card.Header className = "h5">
+                ROS Feed
+            </Card.Header>
+            <Card.Body>
+                <InputGroup.Text id = "ROSFeed" className = "feed">
+                </InputGroup.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
 function servosActuator() {
     const [servos, setCupServos] = useState([useRef(), useRef(), useRef(), useRef(), useRef()])
     const [actuatorVal, setActuator] = useState(useRef())
+    
+    var cmdClient = new ROSLIB.Service({
+        ros: ros,
+        name: '/bio/bio_command',
+        serviceType: '/bio_relay/BioCommand'
+    });
 
     return(
         <Card>
@@ -120,6 +165,12 @@ function fansPumps() {
     const purgeButtonClick = () => {
         console.log("Purging pumps...")
     };
+    
+    var cmdClient = new ROSLIB.Service({
+        ros: ros,
+        name: '/bio/bio_command',
+        serviceType: '/bio_relay/BioCommand'
+    });
 
     return(
         <Card>
@@ -148,7 +199,27 @@ function fansPumps() {
 
 function carousel() {
     const [pos, setPos] = useState(0.0)
-    const [targets, setTargets] = useState([useRef(), useRef()])
+
+    const sendCommand = (cmd) => {
+        var request = new ROSLIB.ServiceRequest({
+          command: cmd
+        });
+        cmdClient.callService(request);
+    }
+
+    const autoRelCmd = () => {
+        var from = document.getElementById("autoFrom")
+        var fromVal = from.options[from.selectedIndex].value
+        fromVal = positions[fromVal]
+        var to = document.getElementById("autoTo")
+        var toVal = to.options[to.selectedIndex].value
+        toVal = positions[toVal]
+
+        if (fromVal != 0 && toVal != 0) {
+            setPos(toVal - fromVal);
+            sendCommand("cr " + pos);
+        }
+    }
 
     const autoRelButtonClick = () => {
         var from = document.getElementById("autoFrom")
@@ -156,16 +227,46 @@ function carousel() {
         var to = document.getElementById("autoTo")
         var toText = to.options[to.selectedIndex].text
 
-        console.log("Targetting relatively from " + fromText + " to " + toText + "...")
+        console.log("Targetting from " + fromText + " to " + toText + "...")
+
+        autoRelCmd()
+    };
+
+    const absTargButtonClick = () => {
+        var absTarg = document.getElementById("absTarget")
+        var absTargVal = absTarg.value
+
+        console.log("Absolute Target is " + absTargVal + "...")
+
+        setPos(absTargVal)
+        sendCommand("ca " + pos);
+    };
+
+    const relTargButtonClick = () => {
+        var relTarg = document.getElementById("relTarget")
+        var relTargVal = relTarg.value
+
+        console.log("Relative Target is " + relTargVal + "...")
+
+        setPos(relTargVal)
+        sendCommand("cr " + pos);
     };
 
     const prevCuvButtonClick = () => {
         console.log("Going to previous cuvette...")
+        sendCommand("cr -1")
     };
 
     const nextCuvButtonClick = () => {
         console.log("Going to next cuvette...")
+        sendCommand("cr 1")
     };
+    
+    var cmdClient = new ROSLIB.Service({
+        ros: ros,
+        name: '/bio/bio_command',
+        serviceType: '/bio_relay/BioCommand'
+    });
 
     return(
         <Card>
@@ -180,29 +281,55 @@ function carousel() {
                             value = {pos}
                         readOnly/>
                     </InputGroup>
-                    {inputGo("Absolute Target", targets[0], "137px")}
-                    {inputGo("Relative Target", targets[1], "137px")}
+                    <InputGroup>
+                        <InputGroup.Text style = {{width: "137px"}}>Absolute Target</InputGroup.Text>
+                        <Form.Control 
+                            id = "absTarget"
+                            defaultValue = "0.0"
+                            type = "number"
+                        />
+                        <Button 
+                            className = "btn-success"
+                            onClick = {absTargButtonClick}
+                        >
+                            Go
+                        </Button>
+                    </InputGroup>
+                    <InputGroup>
+                        <InputGroup.Text style = {{width: "137px"}}>Relatively Target</InputGroup.Text>
+                        <Form.Control 
+                            id = "relTarget"
+                            defaultValue = "0.0"
+                            type = "number"
+                        />
+                        <Button 
+                            className = "btn-success"
+                            onClick = {relTargButtonClick}
+                        >
+                            Go
+                        </Button>
+                    </InputGroup>
                     <InputGroup>
                         <InputGroup.Text style = {{width: "137px"}}>Auto Relative</InputGroup.Text>
                         <Form.Select id = "autoFrom">
-                            <option value = "From">From</option>
-                            <option value = "Separator 1">Separator 1</option>
-                            <option value = "Separator 1">Separator 2</option>
-                            <option value = "Separator 1">Separator 3</option>
-                            <option value = "Dosing 1">Dosing 1</option>
-                            <option value = "Dosing 2">Dosing 2</option>
-                            <option value = "Capping">Capping</option>
-                            <option value = "Observation">Observation</option>
+                            <option value = "0">From</option>
+                            <option value = "separator1">Separator 1</option>
+                            <option value = "separator2">Separator 2</option>
+                            <option value = "separator3">Separator 3</option>
+                            <option value = "dosing1">Dosing 1</option>
+                            <option value = "dosing2">Dosing 2</option>
+                            <option value = "capping">Capping</option>
+                            <option value = "observation">Observation</option>
                         </Form.Select>
                         <Form.Select id = "autoTo">
-                            <option value = "To">To</option>
-                            <option value = "Separator 1">Separator 1</option>
-                            <option value = "Separator 1">Separator 2</option>
-                            <option value = "Separator 1">Separator 3</option>
-                            <option value = "Dosing 1">Dosing 1</option>
-                            <option value = "Dosing 2">Dosing 2</option>
-                            <option value = "Capping">Capping</option>
-                            <option value = "Observation">Observation</option>
+                            <option value = "0">To</option>
+                            <option value = "separator1">Separator 1</option>
+                            <option value = "separator2">Separator 2</option>
+                            <option value = "separator3">Separator 3</option>
+                            <option value = "dosing1">Dosing 1</option>
+                            <option value = "dosing2">Dosing 2</option>
+                            <option value = "capping">Capping</option>
+                            <option value = "observation">Observation</option>
                         </Form.Select>
                         <Button 
                             className="btn-success"
@@ -237,6 +364,9 @@ function BiosensorControl() {
                 {carousel()}
                 {fansPumps()}
                 {servosActuator()}
+            </div>
+            <div className = "card-deck">
+                {rosFeed()}
             </div>
         </Container>
     );
