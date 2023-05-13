57d763f46495668597fdef6e0eef31d32a89c2f3
Nick <nbm0012@uah.edu>
2023-05-12 21:30:31 -0500

    More controller stuff done

--- src/utilities/userController.jsx
+++ src/utilities/userController.jsx
@@ -1,10 +1,21 @@
-import React, {useEffect, useState} from "react";
-
-function useController(){
+import React, {useContext, useEffect, useState} from "react";
+import { RosContext } from "./ROS/RosContext";
+function useController(selectedController = null){
     const [leftInput, setLeftInput] = useState(0);
     const [rightINput, setRightInput] = useState(0);
-    const [selectedController, setSelectedController] = useState(null);
+    const [rosState] = useContext(RosContext);
 
+    useEffect(()=>{
+        if(selectedController){
+            const interval = setInterval(()=>{
+                const controller = navigator.getGamepads()[selectedController];
+                setLeftInput(controller.axes[0]);
+                setRightInput(controller.axes[1]);
+            },50);
+        }
+        console.log(leftInput);
+        console.log(rightINput);
+    },[]);
     
 }
 
